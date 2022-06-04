0629843dbc944f0978b9386753b380136b5ff770
Naihua Chen <naihua.chen@mail.goldennet.com.tw>
2022-06-04 23:39:51 +0800

    [Update--Modify 修改外觀]

--- src/view/HomePage.jsx
+++ src/view/HomePage.jsx
@@ -32,8 +32,8 @@ const HomePage = () => {
                         ProductData.data.map((item, index) => {
                             return (
                                 <>
-                                    <li className={`col-5 product-item ${item.tag ? 'tag' : ''}`} key={index}>
-                                        <img src={require(`../assets/images/product/${item.name}.JPG`)} className='img-fluid' width='150px' alt={item.name} />
+                                    <li className={`col-7 col-md-3 product-item ${item.tag ? 'tag' : ''}`} key={index}>
+                                        <img src={require(`../assets/images/product/${item.name}.JPG`)} className='img-fluid' width='300px' alt={item.name} />
                                         <div className='mt-2'>
                                             <h5 className='text-center fw-bolder py-2'>{item.name}</h5>
                                             <div className='text-start'>
@@ -55,7 +55,7 @@ const HomePage = () => {
             <div className='p-3 mt-2 info-box'>
                 <h4 className='fw-bolder text-center text-brown'>資訊站</h4>
                 {/* button list */}
-                <div className='row mt-3' onClick={handleEvent.changeInfoButton}>
+                <div className='row mt-3 col-12 col-lg-8 mx-auto' onClick={handleEvent.changeInfoButton}>
                     <div className="col-4">
                         <button className={`btn ${infoOption == '運費' ? 'btn-primary text-light' : 'btn-outline-primary text-brown'} w-100 rounded-pill fw-bolder`} value='運費'>運費</button>
                     </div>
@@ -63,11 +63,11 @@ const HomePage = () => {
                         <button className={`btn ${infoOption == '匯款' ? 'btn-primary text-light' : 'btn-outline-primary text-brown'} w-100 rounded-pill fw-bolder`} value='匯款'>匯款</button>
                     </div>
                     <div className="col-4">
-                        <button className={`btn ${infoOption == '聯絡我們' ? 'btn-primary text-light' : 'btn-outline-primary text-brown'} w-100 rounded-pill fw-bolder`} value='聯絡'>聯絡</button>
+                        <button className={`btn ${infoOption == '聯絡' ? 'btn-primary text-light' : 'btn-outline-primary text-brown'} w-100 rounded-pill fw-bolder`} value='聯絡'>聯絡</button>
                     </div>
                 </div>
                 {/* 內容 */}
-                <div className='m-2 bg-secondary rounded content-box'>
+                <div className='col-12 col-md-6 m-2 bg-secondary rounded content-box mx-auto'>
                     { //運費
                         infoOption == '運費' &&
                         <div className='p-3'>
@@ -117,14 +117,14 @@ const HomePage = () => {
                             <ul className='list-unstyled mt-3'>
                                 <li>
                                     <p className='bg-primary text-light rounded py-1 px-5 text-center mb-2' style={{ fontSize: '18px' }}>社群聊聊</p>
-                                    <a href="https://lin.ee/xOtVwFW" target='_blank'><img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入葳茗官方" className='mt-2' height="36" border="0" /></a>
-                                    <br/>
-                                    <a href="https://www.facebook.com/%E8%91%B3%E8%8C%97%E9%A4%9B%E9%A3%A9-116435126424428" className='btn bg-fb-blue text-light fw-bolder mt-2' target='_blank'> Facebook 粉絲專頁</a>
+                                    <a href="https://lin.ee/xOtVwFW" target='_blank'><img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入葳茗官方" className='mt-2' height="46" border="0" /></a>
+                                    <p className='fw-bolder text-red mt-1'>加入Line 官方好友更方便！！</p>
+                                    {/* <a href="https://www.facebook.com/%E8%91%B3%E8%8C%97%E9%A4%9B%E9%A3%A9-116435126424428" className='btn bg-fb-blue text-light fw-bolder mt-2' target='_blank'> Facebook 粉絲專頁</a> */}
                                 </li>
                                 <li className='mt-3'>
                                     <p className='bg-primary text-light rounded py-1 px-5 text-center mb-2' style={{ fontSize: '18px' }}>連絡電話</p>
-                                    <p className='fw-bolder my-2'><FontAwesomeIcon icon={faPhoneSquareAlt} className='me-1 fs-5 text-brown' /> 0955905158 / 陳小姐</p>
-                                    <p className='fw-bolder my-2'><FontAwesomeIcon icon={faPhoneSquareAlt} className='me-1 fs-5 text-brown' /> 0936353171 / 陳先生</p>
+                                    <p className='fw-bolder my-2'><FontAwesomeIcon icon={faPhoneSquareAlt} className='me-1 fs-5 text-brown' /> <a href="tel:+886-955905158">0955905158</a> / 陳小姐</p>
+                                    <p className='fw-bolder my-2'><FontAwesomeIcon icon={faPhoneSquareAlt} className='me-1 fs-5 text-brown' /> <a href="tel:+886-936353171">0936353171</a> / 陳先生</p>
                                 </li>
                             </ul>
                             <hr />
@@ -150,7 +150,7 @@ const HomePage = () => {
                 </div>
             </div>
             {/* 價目表 */}
-            <div className='p-3 mt-2 bg-secondary'>
+            <div className='p-3 mt-2 bg-secondary text-center'>
                 <h4 className='fw-bolder text-center text-brown mb-3'>價目表</h4>
                 <img src={require('../assets/images/price-table.jpg')} className='img-fluid' alt='價目表' />
                 <p className='text-center mt-3'>歡迎下載儲存照片~~</p>
